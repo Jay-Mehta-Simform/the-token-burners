596c298ac9bdd454f6a7a30b75452c29fc5294c1
Pulkit Sharma <122265919+Pulkit-Simform@users.noreply.github.com>
2026-06-13 11:08:51 +0530

    feat: added docker settings

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from "../../generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+const adapter = new PrismaPg();
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
